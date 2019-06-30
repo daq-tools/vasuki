fadeed1b05e1932ed783271e4fa801f40e61d330
Andreas Motl <andreas@terkin.org>
2019-06-30 04:21:03 +0200

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 
 setup(name='vasuki',
-      version='0.2.0',
+      version='0.3.0',
       description='Vasuki generates different kinds of random unique identifiers, tokens and words',
       long_description=README,
       license="AGPL 3, EUPL 1.2",

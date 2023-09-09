2c89c704b1aa61accc85b8a740e3b4882872b2db
Andreas Motl <andreas@terkin.org>
2023-09-09 20:48:02 +0200

    Naga19: Stop using deprecated functions

--- vasuki/__init__.py
+++ vasuki/__init__.py
@@ -4,7 +4,7 @@ __version__ = '0.6.0'
 
 from vasuki.unique.uuid import generate_uuid4
 from vasuki.unique.ulid import generate_ulid
-from vasuki.unique.naga19 import generate_nagamani19, generate_nagamani19_int
+from vasuki.unique.naga19 import generate_nagamani19_hash, generate_nagamani19_int
 from vasuki.unique.gibberish import generate_gibberish
 from vasuki.unique.moment import generate_momentname
 from vasuki.format.sixnibblename.sixnibblename import integer_slug

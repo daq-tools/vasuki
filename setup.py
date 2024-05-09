c52539dc4a82a97b50dc5724224b5e56dfd1e827
Andreas Motl <andreas@terkin.org>
2024-05-09 22:48:58 +0200

    Chore: Update development dependency `poethepoet`

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(name='vasuki',
           'develop': [
               'build<2',
               'bump2version==1.0.1',
-              'poethepoet<0.26',
+              'poethepoet<0.27',
               'twine<6',
           ],
           'service': [
